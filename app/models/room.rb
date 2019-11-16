8f8461da7bf59cb1b8b9332d7ce5a3e3eeccef3c
mahi <mohiddinoshim@yahoo.com>
2019-11-16 23:00:45 +0600

    Create Room Controller
    T_05_C_03

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,9 +3,9 @@ class Room < ApplicationRecord
 
   validates :home_type, presence: true
   validates :room_type, presence: true
-  validates :home_type, presence: true
-  validates :home_type, presence: true
-  validates :home_type, presence: true
+  validates :accommodate, presence: true
+  validates :bed_room, presence: true
+  validates :bath_room, presence: true
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   root 'pages#home'
   devise_for :users,
   			path: '',
@@ -6,4 +7,14 @@ Rails.application.routes.draw do
   			controllers: {omniauth_callbacks: 'omniauth_callbacks',registrations: "registrations"}
 	
 	resources :users, only: [:show] 
+	resources :rooms, except: [:edit] do
+		member do
+			get 'listing' 
+			get 'pricing' 
+			get 'description' 
+			get 'photo_upload' 
+			get 'amenities' 
+			get 'location' 
+		end
+	end
 end
